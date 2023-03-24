228f7112ba644aac52cd28da9651083db282c975
julias42 <artyom-zhulikov@yandex.ru>
2023-03-24 15:00:10 +0700

    read exerxise_one homework2

--- homework2/exercise_one.php
+++ homework2/exercise_one.php
@@ -3,17 +3,12 @@ $question = readline("В каком году произошло крещение
 
 while($question != 990 && $question != 810 && $question != 740){
   $question = readline("В каком году произошло крещение руси? ");
-  break;
 }
 
-if($question == 810){
+if($question == 810 || $question == 740){
       echo "Ответ не верный!";
     }
 
-  if($question == 740){
-    echo "Ответ не верный!";
-  }
-    
   if($question == 990){
     echo "Молодец, ответ верный!";
 }
