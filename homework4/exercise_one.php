a60a6751449647c8a77752b5e36fb60561dbf166
julias42 <artyom-zhulikov@yandex.ru>
2023-04-04 15:03:26 +0700

    complete homework4 exercise_one

--- homework4/exercise_one.php
+++ homework4/exercise_one.php
@@ -1,1 +1,15 @@
 <?php
+
+$array  = [4, 5, 1, 4, 7, 8, 15, 6, 71, 45, 2];
+
+$res = array_map(function(int $array){
+  return $array % 2;
+},$array);
+
+for($i = 0;$i <= count($array); $i++){
+ if($res[$i] == 0){
+  $res[$i] = "Четное";
+} else $res[$i] = "Нечетное";
+};
+
+print_r($res);
