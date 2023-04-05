dda2102bff7e8b9d780cc3a9544596ea93436913
julias42 <artyom-zhulikov@yandex.ru>
2023-04-05 14:44:40 +0700

    read exercise_one

--- homework4/exercise_one.php
+++ homework4/exercise_one.php
@@ -4,12 +4,10 @@ $array  = [4, 5, 1, 4, 7, 8, 15, 6, 71, 45, 2];
 
 $res = array_map(function(int $array){
   return $array % 2;
-},$array);
+}, $array);
 
-for($i = 0;$i <= count($array); $i++){
- if($res[$i] == 0){
-  $res[$i] = "Четное";
-} else $res[$i] = "Нечетное";
+foreach($array as $key => $value){
+ $res[$key] == 0? $res[$key] ="Чётное" : $res[$key] ="Нечётное";
 };
 
 print_r($res);
